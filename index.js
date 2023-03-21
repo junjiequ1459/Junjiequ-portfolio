2680ad60387b8c444cfdd8a11f5f2ce13ce7bf6d
rexqu <junjiequ123@gmail.com>
2023-03-20 20:44:06 -0400

    fix render

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
